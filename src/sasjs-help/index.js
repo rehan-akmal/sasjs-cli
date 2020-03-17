fb65e469fcb7b16d3e708ddf25510cd4e484fdd2
Krishna Acondy <kc.acondy@outlook.com>
2020-03-17 20:09:58 +0000

    Feat: Add aliases for commands

--- src/sasjs-help/index.js
+++ src/sasjs-help/index.js
@@ -9,10 +9,12 @@ export async function printHelpText() {
       "create <foldername>"
     )} - creates the folder structure specified in config.json, inside the provided parent folder
          e.g. sasjs create my-sas-project
+       - if no foldername is specified, it creates the folder structure in the current working directory.
+       - If this is an existing NPM project, it will update package.json with the macrocore dependency.
     * ${chalk.greenBright("help")} - displays this help text
     * ${chalk.greenBright(
-      "build"
+      "build <buildTargetName>"
     )} - builds the services specified in the ${chalk.cyanBright(
     "buildFolders"
-  )} section of the config`);
+  )} section of the config for the specified build target. If none is specified, it will build the first target present in config.json.`);
 }
