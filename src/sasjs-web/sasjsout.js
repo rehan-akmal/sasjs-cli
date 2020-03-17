d6078d67512ee81199160755fb927a5019c80ad0
Krishna Acondy <kc.acondy@outlook.com>
2020-03-17 19:00:52 +0000

    chore: update sasjsout

--- src/sasjs-web/sasjsout.js
+++ src/sasjs-web/sasjsout.js
@@ -1,5 +1,5 @@
 export const sasjsout = `%macro sasjsout(type,fref=sasjs);
-%global sysprocessmode;
+%global sysprocessmode SYS_JES_JOB_URI;
 %if "&sysprocessmode"="SAS Object Server" %then %do;
   %if &type=HTML %then %do;
     filename _webout filesrvc parenturi="&SYS_JES_JOB_URI" name="_webout.json"
@@ -9,11 +9,17 @@ export const sasjsout = `%macro sasjsout(type,fref=sasjs);
     filename _webout filesrvc parenturi="&SYS_JES_JOB_URI" name='_webout.js'
       contenttype='application/javascript';
   %end;
+  %else %if &type=CSS %then %do;
+    filename _webout filesrvc parenturi="&SYS_JES_JOB_URI" name='_webout.js'
+      contenttype='text/css';
+  %end;
 %end;
 %else %do;
-
   %if &type=JS %then %do;
     %let rc=%sysfunc(stpsrv_header(Content-type,application/javascript));
+  %end;
+  %else %if &type=CSS %then %do;
+    %let rc=%sysfunc(stpsrv_header(Content-type,text/css));
   %end;
 %end;
 data _null_;
@@ -22,5 +28,4 @@ data _null_;
   input string $CHAR1. @;
   put string $CHAR1. @;
 run;
-%mend;
-`;
+%mend;`;
